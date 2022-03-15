1fed51bc602b399aaf51a0ce00b6494bd1fc2773
mkaychuks <sachufusi@gmail.com>
2022-03-15 20:11:56 +0100

    My first Gradient Box

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -13,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -48,14 +50,22 @@ fun ImageCard(
     title: String,
     modifier: Modifier = Modifier
 ){
-    Card(modifier = modifier.fillMaxWidth(0.5f)
+    Card(modifier = modifier
+        .fillMaxWidth(0.5f)
         .padding(16.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ) {
         Box(modifier = Modifier.height(200.dp)) {
             Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
-            
+
+            Box(modifier = Modifier.fillMaxSize()
+                .background(Brush.verticalGradient(
+                    colors = listOf(Color.Transparent, Color.Black),
+                    startY = 300f
+                ))
+            )
+
             Box(modifier = Modifier
                 .fillMaxSize()
                 .padding(12.dp), contentAlignment = Alignment.BottomStart
