8cc4e1fa5f26f69c1ece8306643996e4d66fd2e5
mkaychuks <sachufusi@gmail.com>
2022-03-15 19:39:37 +0100

    My first Box composable usage

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,8 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +30,11 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                    val painter = painterResource(id = R.drawable.ifeanyi)
+                    val description = "Ifeanyi in Google Meetup"
+                    val title = "Ifeanyi in Google Meetup"
+                    ImageCard(painter = painter, contentDescription = description, title = title )
                 }
             }
         }
@@ -32,16 +42,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column(modifier = Modifier
-        .background(Color.Green)
-        .fillMaxSize(0.5f)
-        .fillMaxWidth()
-        .padding(top = 12.dp, start = 50.dp)
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Card(modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
     ) {
-        Text(text = "Hello world", modifier = Modifier.clickable {  })
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(text = "Hello Modifier")
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
+            
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp), contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
     }
 }
 
@@ -49,6 +69,9 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        val painter = painterResource(id = R.drawable.ifeanyi)
+        val description = "Ifeanyi in Google Meetup"
+        val title = "Ifeanyi in Google Meetup"
+        ImageCard(painter = painter, contentDescription = description, title = title )
     }
 }
