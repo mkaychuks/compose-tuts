6a8602c405bd81fff495abf883743a20e5ce3e76
mkaychuks <sachufusi@gmail.com>
2022-03-16 20:33:39 +0100

    Working with styling texts, AnnotatedStrings

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,25 +3,20 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -33,10 +28,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
 
-                    val painter = painterResource(id = R.drawable.ifeanyi)
-                    val description = "Ifeanyi in Google Meetup"
-                    val title = "Ifeanyi in Google Meetup"
-                    ImageCard(painter = painter, contentDescription = description, title = title )
+                    TextStyling()
                 }
             }
         }
@@ -44,45 +36,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-){
-    Card(modifier = modifier
-        .fillMaxWidth(0.5f)
-        .padding(16.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
+fun TextStyling(){
+    Box(modifier = Modifier
+        .background(color = Color.White)
+        .fillMaxSize()
+    ){
+        Text(text = buildAnnotatedString {
+             withStyle(
+                 style = SpanStyle(
+                     color = Color.Green,
+                     fontSize = 50.sp
+                 )
+             ){
+                 append("I")
+             }
+            append("feanyi ")
 
-            Box(modifier = Modifier.fillMaxSize()
-                .background(Brush.verticalGradient(
-                    colors = listOf(Color.Transparent, Color.Black),
-                    startY = 300f
-                ))
-            )
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp), contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 50.sp
+                )
+            ){
+                append("S")
             }
-        }
+            append("amuel")
+        },
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline
+            )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        val painter = painterResource(id = R.drawable.ifeanyi)
-        val description = "Ifeanyi in Google Meetup"
-        val title = "Ifeanyi in Google Meetup"
-        ImageCard(painter = painter, contentDescription = description, title = title )
+        TextStyling()
     }
 }
