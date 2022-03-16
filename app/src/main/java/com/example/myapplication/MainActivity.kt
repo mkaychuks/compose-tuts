03fe29dcdccc7414be6b08d263ef1ce65b72c785
mkaychuks <sachufusi@gmail.com>
2022-03-16 21:03:16 +0100

    States, Recomposition and all, with a lil twist

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,21 +3,18 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,8 +24,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
-                    TextStyling()
+                    StateManagement()
                 }
             }
         }
@@ -36,36 +32,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TextStyling(){
-    Box(modifier = Modifier
-        .background(color = Color.White)
-        .fillMaxSize()
-    ){
-        Text(text = buildAnnotatedString {
-             withStyle(
-                 style = SpanStyle(
-                     color = Color.Green,
-                     fontSize = 50.sp
-                 )
-             ){
-                 append("I")
-             }
-            append("feanyi ")
+fun StateManagement(){
+    val number = remember{
+        mutableStateOf(0)
+    }
 
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Green,
-                    fontSize = 50.sp
-                )
-            ){
-                append("S")
-            }
-            append("amuel")
-        },
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            textDecoration = TextDecoration.Underline
-            )
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+        Text(text = "Button clicked ${number.value} time(s)")
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = {
+            number.value += 1
+        }) {
+            Text(text = "Increase")
+        }
     }
 }
 
@@ -74,6 +55,6 @@ fun TextStyling(){
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        TextStyling()
+        StateManagement()
     }
 }
