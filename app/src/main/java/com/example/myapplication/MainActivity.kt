cd4749402f4c8cb8ba3b49b913f24cad6e78ab67
mkaychuks <sachufusi@gmail.com>
2022-03-19 21:02:55 +0100

    Implemented ConstraintLayout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,17 +3,17 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LazyColumns()
+                    ConstraintLayouts()
                 }
             }
         }
@@ -31,20 +31,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LazyColumns(){
-    LazyColumn {
-        items(200){
-            Text(
-                text = "Item $it",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-            )
+fun ConstraintLayouts(){
+
+    // creating a ConstraintSet for the composable
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor("greenBox")
+        val redBox = createRefFor("redBox")
+
+        constrain(greenBox){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(redBox.start)
         }
 
+        constrain(redBox){
+            top.linkTo(parent.top)
+            end.linkTo(parent.end)
+            start.linkTo(greenBox.end)
+        }
+    }
+    // making use of the constraints
+    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.background(color = Color.Cyan).layoutId("greenBox"))
+        Box(modifier = Modifier.background(color = Color.Blue).layoutId("redBox"))
     }
 }
 
@@ -53,6 +62,6 @@ fun LazyColumns(){
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        LazyColumns()
+       ConstraintLayouts()
     }
 }
