1ec85c3e81138340c1de2c1a32d8a990f1e45fa6
mkaychuks <sachufusi@gmail.com>
2022-03-22 20:07:29 +0100

    A little intro on Effect Handlers

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,12 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +17,23 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Nonsense()
+                    val scaffoldstate = rememberScaffoldState()
+                    val scope = rememberCoroutineScope()
+                    Scaffold(scaffoldState = scaffoldstate){
+                        var counter = produceState(initialValue = 0){
+                            delay(3000L)
+                            value = 4
+                        }
+                        if(counter.value % 5 == 0 && counter.value > 0){
+                            LaunchedEffect(key1 = scaffoldstate.snackbarHostState){
+                                scaffoldstate.snackbarHostState.showSnackbar("Hello")
+                            }
+                        }
+
+                        Button(onClick = {}) {
+                            Text("Click me: $counter")
+                        }
+                    }
                 }
             }
         }
@@ -31,43 +41,12 @@ class MainActivity : ComponentActivity() {
 }
 
 
-private val constraints = ConstraintSet{
-    val greenBox = createRefFor("greenButton")
-    val redBox = createRefFor("redButton")
-
-    constrain(greenBox){
-        top.linkTo(parent.top)
-        start.linkTo(parent.start)
-        end.linkTo(redBox.start)
-    }
-
-    constrain(redBox){
-        top.linkTo(parent.top)
-        end.linkTo(parent.end)
-        start.linkTo(greenBox.end)
-    }
-}
-
-@Composable
-fun Nonsense(){
-    // making use of the constraints
-    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.background(color = Color.Blue)
-            .layoutId("redButton")
-            .size(50.dp, 50.dp))
-
-        Box(modifier = Modifier.background(color = Color.Red)
-            .layoutId("greenButton")
-            .size(50.dp, 50.dp))
-    }
-}
-
 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-       Nonsense()
+
     }
 }
