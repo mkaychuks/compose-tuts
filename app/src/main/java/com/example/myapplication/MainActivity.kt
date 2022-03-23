ea0b1523db78c34476a70705d1a3f817feef6e0b
mkaychuks <sachufusi@gmail.com>
2022-03-23 19:24:38 +0100

    Animation using spring()

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -43,10 +41,8 @@ fun SimpleAnimation(){
 
     val size by animateDpAsState(
             targetValue = sizeState,
-            tween(
-               durationMillis = 5000,
-                delayMillis = 300,
-                easing = LinearOutSlowInEasing
+            spring(
+                Spring.DampingRatioHighBouncy
             )
         )
 
