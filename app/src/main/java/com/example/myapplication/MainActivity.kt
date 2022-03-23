98c55ed893f4f493773cab4bbf9684ca4197f4c9
mkaychuks <sachufusi@gmail.com>
2022-03-23 19:23:29 +0100

    Animation using tween()

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,21 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +27,37 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    OnBoarding()
+                    SimpleAnimation()
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun SimpleAnimation(){
+    var sizeState by remember {
+        mutableStateOf(200.dp)
+    }
+
+    val size by animateDpAsState(
+            targetValue = sizeState,
+            tween(
+               durationMillis = 5000,
+                delayMillis = 300,
+                easing = LinearOutSlowInEasing
+            )
+        )
+
+    Box(modifier = Modifier
+        .size(size)
+        .background(color = Color.Red),
+        contentAlignment = Alignment.Center
+    ){
+        Button(onClick = { /*TODO*/ }) {
+            sizeState += 50.dp
+            Text(text = "Increase")
         }
     }
 }
