d7c838f873959527d16db819d0e1b529cc60cf45
mkaychuks <sachufusi@gmail.com>
2022-03-28 15:25:15 +0100

    Sign up with Google button with animation

--- app/src/main/java/com/example/myapplication/Exercises.kt
+++ app/src/main/java/com/example/myapplication/Exercises.kt
@@ -2,130 +2,79 @@ package com.example.myapplication
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.Shapes
-
 
 @ExperimentalMaterialApi
 @Composable
-fun OnBoardingAnimatedCard(){
-    var expandedState by remember { mutableStateOf(false)}
+fun GoogleButton(
+    text: String = "Sign Up with Google",
+    loadingText: String = "Creating Account..."
+){
+    var clicked by remember{ mutableStateOf(false) }
 
-    val rotationState by animateFloatAsState(
-        targetValue = if(expandedState) 180f else 0f
-    )
-
-    // parent column
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        onClick = { clicked = !clicked },
+        shape = Shapes.medium,
+        border = BorderStroke(1.dp, color = Color.LightGray),
+        color = MaterialTheme.colors.surface
     ) {
-
-        // main card
-        Card(
-            modifier = Modifier
+        Row(
+            modifier = Modifier.padding(
+                start = 12.dp,
+                end = 16.dp,
+                top = 12.dp,
+                bottom = 12.dp
+            )
                 .animateContentSize(
                     animationSpec = tween(
                         durationMillis = 300,
                         easing = LinearOutSlowInEasing
                     )
-                )
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f)
-                .padding(10.dp),
-            shape = Shapes.medium,
-            onClick = {
-                expandedState = !expandedState
-            }
+                ),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            // column inside card
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                // row inside column, inside the card
-                Row(
+            Icon(painter = painterResource(
+                id = R.drawable.ic_google),
+                contentDescription = "Icon",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = if(clicked) loadingText else text)
+            if(clicked){
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "OnBoarding", fontSize = 24.sp, textAlign = TextAlign.Center, fontWeight = FontWeight.Bold, modifier = Modifier.weight(6f))
-                    IconButton(
-                        onClick = {
-                            expandedState = !expandedState
-                        },
-                        modifier = Modifier
-                            .weight(1f)
-                            .rotate(rotationState)
-                    ) {
-                       Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Drop-Down")
-                    }
-                } // row inside column, inside the card
-
-                if(expandedState){
-                    Details()
-                }
-
-            } // column inside card
-
-        } // main card
-
-    } // parent column
-}
-
-
-@Composable
-fun Details(){
-    OutlinedTextField(
-        value = "",
-        onValueChange ={},
-        label = { Text(text = "Username")},
-        leadingIcon = { Icon(imageVector = Icons.Filled.Person, contentDescription = "Username")}
-    )
-
-    OutlinedTextField(
-        value = "",
-        onValueChange ={},
-        label = { Text(text = "Password")},
-        leadingIcon = { Icon(imageVector = Icons.Filled.Lock, contentDescription = "Password")}
-    )
-    Spacer(modifier = Modifier.height(8.dp))
-
-    OutlinedButton(onClick = { /*TODO*/ }) {
-        Text(text = "LOGIN")
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+        }
     }
 }
 
 
 
 
-
-
-
-
 @ExperimentalMaterialApi
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview1(){
-    OnBoardingAnimatedCard()
+@Preview
+fun DefaultPreviewer(){
+    GoogleButton()
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,16 +18,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    TextFieldsState()
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        GoogleButton()
+                    }
                 }
             }
         }
