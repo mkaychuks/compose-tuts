7093de768d53aa37cf58d8d735bdd91798e948d0
mkaychuks <sachufusi@gmail.com>
2022-03-28 13:54:40 +0100

    More on TextFields

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,35 +1,33 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    OnBoardingAnimatedCard()
+                    TextFieldsState()
                 }
             }
         }
@@ -38,52 +36,49 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun SimpleAnimation(){
-    var sizeState by remember {
-        mutableStateOf(200.dp)
-    }
+fun TextFieldsState(){
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-    val size by animateDpAsState(
-            targetValue = sizeState,
-            tween(
-                durationMillis = 5000,
-                delayMillis = 300,
-                easing = LinearOutSlowInEasing
-            )
-        )
+       // creating a state for the text field
+        var emailState by remember{ mutableStateOf("admin@gmail.com")}
 
-    val infiniteTransition = rememberInfiniteTransition()
-    val color by infiniteTransition.animateColor(
-        initialValue = Color.Red,
-        targetValue = Color.Blue,
-        animationSpec = infiniteRepeatable(
-            tween(durationMillis = 3000, delayMillis = 300, easing = LinearOutSlowInEasing),
-             repeatMode = RepeatMode.Reverse
-        )
-    )
-
-    Box(modifier = Modifier
-        .size(size)
-        .clip(shape = CircleShape)
-        .background(color),
-        contentAlignment = Alignment.Center
-    ){
-        Button(onClick = {
-            sizeState += 50.dp
-        }) {
-            Text(text = "Increase")
-        }
+       OutlinedTextField(
+           value = emailState,
+           onValueChange = { newEmail ->
+                emailState = newEmail
+       },
+           label = { Text(text = "Email")},
+           leadingIcon = {
+               IconButton(onClick = {
+                   Log.d("Leading Icon", "Leading Icon Clicked")
+               }) {
+                   Icon(imageVector = Icons.Filled.Email, contentDescription = "Email form")
+               }
+           },
+           keyboardOptions = KeyboardOptions(
+               imeAction = ImeAction.Done,
+               keyboardType = KeyboardType.Email
+           ),
+           keyboardActions = KeyboardActions(
+               onDone = {
+                   Log.d("ImeAction","onDone click")
+               }
+           )
+       )
     }
 }
 
 
 
 
-@ExperimentalMaterialApi
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        OnBoardingAnimatedCard()
+        TextFieldsState()
     }
 }
