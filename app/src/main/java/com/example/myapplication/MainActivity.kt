e163b581301a896c40398e22fb05d868420177e8
mkaychuks <sachufusi@gmail.com>
2022-03-30 20:18:44 +0100

    Image loading with Coil

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.BlurTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,16 +32,7 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        SignUpForm()
-                        Spacer(modifier = Modifier.height(12.dp))
-                        ThirdPartyButton()
-                        Spacer(modifier = Modifier.height(8.dp))
-                        ThirdPartyButton(
-                            title = "Sign Up with Instagram",
-                            loadingText = "Creating Account",
-                            icon = painterResource(id = R.drawable.ic_google),
-                            background = Color.Transparent
-                        )
+                        CoilImage()
                     }
                 }
             }
@@ -54,40 +41,33 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@ExperimentalCoilApi
 @Composable
-fun TextFieldsState(){
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-       // creating a state for the text field
-        var emailState by remember{ mutableStateOf("admin@gmail.com")}
-
-       OutlinedTextField(
-           value = emailState,
-           onValueChange = { newEmail ->
-                emailState = newEmail
-       },
-           label = { Text(text = "Email")},
-           leadingIcon = {
-               IconButton(onClick = {
-                   Log.d("Leading Icon", "Leading Icon Clicked")
-               }) {
-                   Icon(imageVector = Icons.Filled.Email, contentDescription = "Email form")
-               }
-           },
-           keyboardOptions = KeyboardOptions(
-               imeAction = ImeAction.Done,
-               keyboardType = KeyboardType.Email
-           ),
-           keyboardActions = KeyboardActions(
-               onDone = {
-                   Log.d("ImeAction","onDone click")
-               }
-           )
-       )
+fun CoilImage(){
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .width(150.dp),
+        contentAlignment = Alignment.Center
+    ){
+        val painter = rememberImagePainter(
+            data = "https://www.bing.com/images/search?view=detailV2&ccid=9ZDZ4%2f90&id=218E38B4D2C1199E5C10DE8BEC478DE0691B0345&thid=OIP.9ZDZ4_90qBA093Z5YKOGdgHaEK&mediaurl=https%3a%2f%2fcdn2.expertreviews.co.uk%2fsites%2fexpertreviews%2ffiles%2fstyles%2fer_main_wide%2fpublic%2f2015%2f05%2fgoogle_photos.png%3fitok%3d65cfaqGF&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.f590d9e3ff74a81034f7767960a38676%3frik%3dRQMbaeCNR%252byL3g%26pid%3dImgRaw%26r%3d0&exph=540&expw=960&q=google+photo&simid=608033735780417915&FORM=IRPRST&ck=6AB7CED57FAA27DFC1F0562511756CF8&selectedIndex=7",
+            builder = {
+                placeholder(R.drawable.ic_google)
+                crossfade(1000)
+                transformations(
+                    GrayscaleTransformation(),
+                    RoundedCornersTransformation(50f)
+                )
+            }
+        )
+        val painterState = painter.state
+        Image(painter = painter, contentDescription = "Google Picture")
+        if (painterState is ImagePainter.State.Loading){
+            CircularProgressIndicator()
+        }
     }
+
 }
 
 
@@ -98,6 +78,6 @@ fun TextFieldsState(){
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        TextFieldsState()
+
     }
 }
